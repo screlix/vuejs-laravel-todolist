4ea5a21addf6d5cb827ee5d47dc5adaf14d7bce0
mohamed <medox@mohameds-MacBook-Air.local>
2022-01-23 16:09:44 +0100

    p

--- resources/js/axiosAuth.js
+++ resources/js/axiosAuth.js
@@ -2,5 +2,3 @@ import store from "../js/vue/store/index";
 
 var access_token = store.state.token;
 axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
-axios.defaults.headers.common["Content-Type"] = "application/json";
-axios.defaults.withCredentials = true;
